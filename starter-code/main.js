e38070658cc8e5932fecca0da538cfb75882dd81
arianadziedzic <ariana.dziedzic@gmail.com>
2016-08-11 16:34:26 -0400

    createBoard javascript addition

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,15 +1,25 @@
 console.log("JS file is connected to HTML! Woo!")
 
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+//var cardOne = "queen";
+//var cardTwo = "queen";
+//var cardThree = "king";
+//var cardFour = "king";
 
-  if (cardOne === cardTwo) {
+/* if (cardOne === cardTwo) {
     alert('You found a match!')
   } else {
     alert('Sorry, try again.');
+  }*/
+
+var gameBoard = document.getElementById('game-board');
+var cards = 4;
+var createBoard = function() {
+  for (var i=0; i<cards.length; i++) {
+    var cardElement = document.createElement('div');
+    cardElement.className = 'card';
+    gameBoard.appendChild(cardElement);
   }
+};
 
-
+createBoard()
